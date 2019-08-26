8a53360714c3ac441246d9abc7b1c818abb5c681
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-26 01:36:48 -0700

    added noteform

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import NoteForm from './notes/NoteForm';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <NoteForm title="hi" body="there"/>
+    </>
+  );
+
 }
-  
